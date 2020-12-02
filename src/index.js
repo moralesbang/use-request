504a0a0c3809636eae359490676b054fb44d8cfa
Juan Camilo Morales <camov1209@gmail.com>
2020-12-02 16:50:42 -0500

    add base hooks

--- src/index.js
+++ src/index.js
@@ -1,21 +1,63 @@
-import * as React from 'react'
+import { useReducer, useEffect } from 'react'
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState({
-    counter: 0
+function useSetState(initialState = {}) {
+  const reducer = useReducer((state, newState) => {
+    return { ...state, ...newState }
+  }, initialState)
+
+  return reducer
+}
+
+export function useLazyRequest({
+  service,
+  payload,
+  onSuccess,
+  onFailure,
+  onFetch,
+  transformData = (data) => data,
+  transformError
+}) {
+  const [state, setState] = useSetState({
+    data: null,
+    fetching: false,
+    error: null
   })
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval)
-    }
-  }, [])
+  const fetchData = () => {
+    try {
+      const response = service(payload)
+      const isSuccess = response.status === 200 // Update for handle all 2XX
+      const responseError = transformError(response)
 
-  return counter
+      if (isSuccess) {
+        setState({
+          data: response.data && transformData(response.data),
+          error: null
+        })
+        if (onSuccess) onSuccess(response)
+      } else {
+        setState({ data: null, error: responseError }) // Update for handle every rest client
+        if (onFailure) onFailure(responseError)
+      }
+    } catch (error) {
+      console.error(`🚨 ${error}`)
+    } finally {
+      setState({ fetching: false })
+      if (onFetch) onFetch()
+    }
+  }
+
+  return [state, fetchData, setState]
 }
+
+export function useRequest({ dependencies = [], ...options }) {
+  const lazyRequest = useLazyRequest(options)
+
+  useEffect(() => {
+    lazyRequest.fetchData()
+  }, dependencies)
+
+  return lazyRequest
+}
+
+export const customizeHook = (hook, customOptions) => (options) => hook({ ...customOptions, options })
