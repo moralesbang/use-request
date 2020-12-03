5027ebb0bd8aeb88cf013162b59c570d76d92abe
Juan Camilo Morales <camov1209@gmail.com>
2020-12-03 12:28:22 -0500

    fix use-request

--- src/index.js
+++ src/index.js
@@ -50,13 +50,13 @@ export function useLazyRequest({
 }
 
 export function useRequest({ dependencies = [], ...options }) {
-  const lazyRequest = useLazyRequest(options)
+  const [state, fetchData, setState] = useLazyRequest(options)
 
   useEffect(() => {
-    lazyRequest.fetchData()
+    fetchData()
   }, dependencies)
 
-  return lazyRequest
+  return [state, fetchData, setState]
 }
 
 export const customizeHook = (hook, customOptions) => (options) => hook({ ...customOptions, ...options })
