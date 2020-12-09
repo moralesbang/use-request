b5c7126945fac15176d78f25380dbbe38599c86d
Juan Camilo Morales <camov1209@gmail.com>
2020-12-09 10:44:52 -0500

    fix example and add direct-payload for fetch function

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import { useMyHook } from 'use-request'
+import React, {useState} from 'react'
+import {create} from 'apisauce'
+import { useRequest } from 'use-request'
 
-const App = () => {
-  const example = useMyHook()
+const githubApi = create({
+  baseURL: 'https://api.github.com/',
+  timeout: 3000
+})
+
+const fetchUserRepos = username => githubApi.get(`/users/${username}/repos`)
+
+const handleSubmit = callback => event => {
+  event.preventDefault()
+  callback()
+}
+
+function App () {
+  const [username, setUsername] = useState('')
+  const [repos, getRepos] = useRequest({ service: fetchUserRepos, payload: 'moralesbang' })
+
   return (
     <div>
-      {example}
+      <h2>Repos</h2>
+      <form onSubmit={handleSubmit(() => getRepos(username))}>
+        <input name='username' type='text' onChange={(event) => setUsername(event.target.value)} />
+        <button type='submit'>Request</button>
+      </form>
+      { repos.data?.length ? (
+        <>
+          <ul>
+            {repos.data?.map(repo => <li key={repo.id}>{repo.name}</li>)}
+          </ul></>
+      ) : <span role='img' aria-label='smiley emoji'>😄</span> }
     </div>
   )
 }
+
 export default App
