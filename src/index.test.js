414fcc304256dbda00aac859699e1624bc7bd009
Juan Camilo Morales <camov1209@gmail.com>
2021-02-22 22:14:46 -0500

    refactor some tests

--- src/index.test.js
+++ src/index.test.js
@@ -9,15 +9,20 @@ const api = create({
 const successfulService = () => api.get('/successful')
 const failureService = () => api.post('/failure')
 
+const requiredServiceError = Error('🚨 You must to provide a valid service')
+const expectedFetchingState = { data: null, status: 'fetching', error: null }
+const expectedSuccessfulState = { data: { message: 'Hello World' }, status: 'success', error: null }
+const expectedErrorState = { data: null, status: 'error', error: CLIENT_ERROR }
+
 describe('useLazyRequest', () => {
   test('instance with no options', () => {
     const {result} = renderHook(() => useLazyRequest())
-    expect(result.error).toEqual(Error('🚨 You must to provide a valid service'))
+    expect(result.error).toEqual(requiredServiceError)
   })
 
   test('instance with no service', () => {
     const {result} = renderHook(() => useLazyRequest({}))
-    expect(result.error).toEqual(Error('🚨 You must to provide a valid service'))
+    expect(result.error).toEqual(requiredServiceError)
   })
 
   test('request service is successful with status', async () => {
@@ -32,12 +37,12 @@ describe('useLazyRequest', () => {
     })
 
     const [fetchingState] = result.current
-    expect(fetchingState).toMatchObject({ data: null, status: 'fetching', error: null })
+    expect(fetchingState).toMatchObject(expectedFetchingState)
 
     await waitForNextUpdate()
 
     const [successfulState] = result.current
-    expect(successfulState).toMatchObject({ data: { message: 'Hello World' }, status: 'success', error: null })
+    expect(successfulState).toMatchObject(expectedSuccessfulState)
   })
 
   test('request service is failure', async () => {
@@ -52,42 +57,42 @@ describe('useLazyRequest', () => {
 
     const [state] = result.current
 
-    expect(state).toMatchObject({ data: null, status: 'error', error: CLIENT_ERROR })
+    expect(state).toMatchObject(expectedErrorState)
   })
 })
 
 describe('useRequest', () => {
   test('no options', () => {
     const {result} = renderHook(() => useRequest())
-    expect(result.error).toEqual(Error('🚨 You must to provide a valid service'))
+    expect(result.error).toEqual(requiredServiceError)
   })
 
   test('instance with no service', () => {
     const {result} = renderHook(() => useRequest({}))
-    expect(result.error).toEqual(Error('🚨 You must to provide a valid service'))
+    expect(result.error).toEqual(requiredServiceError)
   })
 
   test('request service is successful', async () => {
     const {result, waitForNextUpdate} = renderHook(() => useRequest({ service: successfulService }))
 
     const [fetchingState] = result.current
-    expect(fetchingState).toMatchObject({ data: null, status: 'fetching', error: null })
+    expect(fetchingState).toMatchObject(expectedFetchingState)
 
     await waitForNextUpdate()
 
     const [successfulState] = result.current
-    expect(successfulState).toMatchObject({ data: { message: 'Hello World' }, status: 'success', error: null })
+    expect(successfulState).toMatchObject(expectedSuccessfulState)
   })
 
   test('request service is failure', async () => {
     const {result, waitForNextUpdate} = renderHook(() => useRequest({ service: failureService }))
 
     const [fetchingState] = result.current
-    expect(fetchingState).toMatchObject({ data: null, status: 'fetching', error: null })
+    expect(fetchingState).toMatchObject(expectedFetchingState)
 
     await waitForNextUpdate()
 
     const [failureState] = result.current
-    expect(failureState).toMatchObject({ data: null, status: 'error', error: CLIENT_ERROR })
+    expect(failureState).toMatchObject(expectedErrorState)
   })
 })
